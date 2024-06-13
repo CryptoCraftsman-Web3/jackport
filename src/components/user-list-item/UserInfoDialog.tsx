66dff733a28cca3a5cbfc51d1b30e23d4f5ec333
CryptoCraftsman-Web3 <taylormoore97521@gmail.com>
2024-06-13 06:36:44 -0700

    user info dialog

---
+++ src/components/user-list-item/UserInfoDialog.tsx
@@ -0,0 +1,172 @@
+import {
+    Button,
+    Dialog,
+    Grid,
+    Card,
+    CardContent,
+    Typography,
+    Divider,
+  } from "@mui/material";
+import SolLogo from "../SolLogo";
+import React, {useEffect}  from "react";
+import DialogContent from '@mui/material/DialogContent';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+import { generateFromString } from "generate-avatar";
+import SportsBasketballIcon from '@mui/icons-material/SportsBasketball';
+import DiamondIcon from '@mui/icons-material/Diamond';
+import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium';
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
+  type Props = {
+    open: boolean;
+    handleClose: () => void;
+    name: string
+  };
+
+  function createData(
+    value: string,
+    category: string,
+    badge: React.ReactNode,
+  ) {
+    return { value, category, badge };
+  }
+  
+  const rows = [
+    createData("6438", "Played", <SportsBasketballIcon />),
+    createData("735", "Won", <DiamondIcon />),
+    createData("9.99", "Biggest Win", <EmojiEventsIcon />),
+    createData("298x", "Luckiest Win", <FaceRetouchingNaturalIcon />)
+  ];
+  
+  const UserInfoDialog = ({ open, handleClose, name }: Props) => {
+    const theme = useTheme();
+    const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
+    return (
+      <Dialog
+        fullScreen={fullScreen}        
+        open={open}
+        scroll="body"
+        onClose={handleClose}
+        sx={{ p: 5 }}
+        PaperProps={{ sx: { p: 4 } }}
+      >
+        <IconButton
+            aria-label="close"
+            onClick={handleClose}
+            sx={{
+                position: 'absolute',
+                right: 8,
+                top: 8,
+                color: (theme) => theme.palette.grey[500],
+            }}>
+            <CloseIcon />
+        </IconButton>
+        <DialogContent  sx={{padding: 0}}>
+            <Grid container 
+                  spacing={2}   
+                  direction="column"
+                  justifyContent="center"
+                  alignItems="stretch">
+                <Grid item>
+                    <div className="flex flex-col items-center">
+                        <img
+                            src={`data:image/svg+xml;utf8,${generateFromString(name)}`}
+                            className="w-20 h-20 rounded-full"
+                        />
+                        <div className="flex items-center pt-3">
+                            <Typography>{name.slice(0, 3) + "..." + name.slice(-3)}</Typography>
+                            <WorkspacePremiumIcon />
+                        </div>
+                    </div>
+                </Grid>
+                <Grid item container direction="column" alignItems="stretch" spacing={1}>
+                    <Grid item>
+                        <div className="flex justify-between items-center">
+                            <Typography fontWeight="800" color={"grey"}>Past Rounds</Typography>
+                            <Button endIcon={<OpenInNewIcon />}>View Wallet</Button>
+                        </div>
+                    </Grid>
+                    <Grid item>
+                        <Card className="w-100 border" sx={{borderRadius: "20px", borderColor: "#555"}}>
+                            <Grid container>
+                                {rows.map((row, index) => (
+                                    <React.Fragment>
+                                        <Grid item xs={12} md padding={2}>
+                                            <div className="h-full flex flex-col justify-between">
+                                                <Typography variant="h5">{row.value}</Typography>
+                                                <div className="flex items-center pt-2">
+                                                    <Typography color={"grey"} >{row.category}</Typography>
+                                                    <div className="ps-2">{row.badge}</div>
+                                                </div>
+                                            </div>
+                                        </Grid>
+                                        {index < rows.length - 1 && <Divider orientation="vertical" flexItem />}
+                                    </React.Fragment>
+                                ))}
+                            </Grid>
+                        </Card>
+                    </Grid>
+                </Grid>
+                <Grid item>
+                    <Divider />
+                </Grid>
+                <Grid item container spacing={1}>
+                    <Grid item>
+                        <Typography fontWeight="800" color={"grey"}>This Round</Typography>
+                    </Grid>
+                    <Grid item container direction="column" spacing={2}>
+                        <Grid item>
+                            <Card sx={{borderRadius: "20px"}}>
+                                <CardContent>
+                                    <Grid item container spacing={2}>
+                                        <Grid xs={12} md item>
+                                            <div>
+                                                <div className="flex items-center">
+                                                    <SolLogo />
+                                                    <Typography variant="h5" px={1}>0.1</Typography>
+                                                </div>
+                                                <Typography color="grey">Entries' Value</Typography>
+                                            </div>
+                                        </Grid>
+                                        <Grid xs={12} md item>
+                                            <div>
+                                                <Typography variant="h5">50%</Typography>
+                                                <Typography color="grey" >Win Chance</Typography>
+                                            </div>
+                                        </Grid>
+                                    </Grid>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                        <Grid item container alignItems="center" px={1}>
+                            <Grid xs item>
+                                <div className="flex items-center">
+                                    <SolLogo />
+                                    <Typography variant="h5" px={1} >0.1 SOL</Typography>
+                                </div>
+                            </Grid>
+                            <Grid xs item>
+                                <div className="flex flex-col items-end">
+                                    <div className="flex items-center">
+                                        <SolLogo />
+                                        <Typography px={1}>0.1</Typography>
+                                    </div>
+                                    <Typography color="grey" >$360.87</Typography>
+                                </div>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </DialogContent>
+      </Dialog>
+    );
+  };
+  
+  export default UserInfoDialog;
+  
--- src/components/user-list-item/index.tsx
+++ src/components/user-list-item/index.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import { Player } from "../../utils/type";
 import { LAMPORTS_PER_SOL } from "@solana/web3.js";
 import { generateFromString } from "generate-avatar";
 import { useWallet } from "@solana/wallet-adapter-react";
+import UserInfoDialog from "./UserInfoDialog"
 
 type Props = {
   item: Player;
@@ -14,61 +15,65 @@ type Props = {
 
 const UserListItem = ({ item, sumPots, hovered, winner, onHover }: Props) => {
   const wallet = useWallet();
-  // console.log("win==", winner);
+  const [open, setOpen] = useState<boolean>(false);
   return (
-    <div
-      onMouseEnter={() => onHover(item.id)}
-      onMouseLeave={() => onHover(NaN)}
-      className="flex items-center justify-between mb-2 p-3 rounded-2xl relative cursor-pointer hover:scale-105 duration-300"
-      style={{
-        border: `1px solid ${item.color}`,
-        opacity: !Number.isNaN(hovered)
-          ? hovered === item.id
-            ? 1
-            : 0.2
-          : winner
-          ? winner === item.player
-            ? 1
-            : 0.2
-          : 1,
-        background: winner
-          ? winner === item.player
-            ? "linear-gradient(111deg, rgba(253,202,45,0.3477766106442577) 0%, rgba(175,195,34,0.0844712885154062) 85%)"
-            : "none"
-          : "none",
-      }}
-    >
-      <img
-        src={`data:image/svg+xml;utf8,${generateFromString(item.player)}`}
-        className="w-8 h-8 rounded-full"
-      />
-
-      <div className="flex-1 ml-2">
-        <div className="text-sm">
-          {wallet.publicKey?.toBase58() === item.player
-            ? "You"
-            : item.player.slice(0, 3) + "..." + item.player.slice(-3)}
-          {winner === item.player && (
-            <img className="inline-block ml-3" src="/crown.png" width={16} />
-          )}
-        </div>
-        <div className="text-xs text-gray-400">
-          {(item.amount / LAMPORTS_PER_SOL).toLocaleString()} SOL
-        </div>
-      </div>
-      <div className="text-right mr-6">
-        <div className="text-sm">
-          {Math.round(
-            (((item.amount / LAMPORTS_PER_SOL) * 100) / sumPots) * 100
-          ) / 100}
-          %
-        </div>{" "}
-      </div>
+    <>
       <div
-        className="absolute right-2 w-6 h-6 rounded-full"
-        style={{ backgroundColor: item.color }}
-      />
-    </div>
+        onMouseEnter={() => onHover(item.id)}
+        onMouseLeave={() => onHover(NaN)}
+        onClick={() => setOpen(true)}
+        className="flex items-center justify-between mb-2 p-3 rounded-2xl relative cursor-pointer hover:scale-105 duration-300"
+        style={{
+          border: `1px solid ${item.color}`,
+          opacity: !Number.isNaN(hovered)
+            ? hovered === item.id
+              ? 1
+              : 0.2
+            : winner
+            ? winner === item.player
+              ? 1
+              : 0.2
+            : 1,
+          background: winner
+            ? winner === item.player
+              ? "linear-gradient(111deg, rgba(253,202,45,0.3477766106442577) 0%, rgba(175,195,34,0.0844712885154062) 85%)"
+              : "none"
+            : "none",
+        }}
+      >
+        <img
+          src={`data:image/svg+xml;utf8,${generateFromString(item.player)}`}
+          className="w-8 h-8 rounded-full"
+        />
+
+        <div className="flex-1 ml-2">
+          <div className="text-sm">
+            {wallet.publicKey?.toBase58() === item.player
+              ? "You"
+              : item.player.slice(0, 3) + "..." + item.player.slice(-3)}
+            {winner === item.player && (
+              <img className="inline-block ml-3" src="/crown.png" width={16} />
+            )}
+          </div>
+          <div className="text-xs text-gray-400">
+            {(item.amount / LAMPORTS_PER_SOL).toLocaleString()} SOL
+          </div>
+        </div>
+        <div className="text-right mr-6">
+          <div className="text-sm">
+            {Math.round(
+              (((item.amount / LAMPORTS_PER_SOL) * 100) / sumPots) * 100
+            ) / 100}
+            %
+          </div>{" "}
+        </div>
+        <div
+          className="absolute right-2 w-6 h-6 rounded-full"
+          style={{ backgroundColor: item.color }}
+        />
+      </div>
+      <UserInfoDialog open={open} handleClose={() => setOpen(!open)} name={item.player} />
+    </>
   );
 };
 
